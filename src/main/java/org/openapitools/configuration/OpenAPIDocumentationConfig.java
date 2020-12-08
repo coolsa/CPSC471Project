5a32d2b4a7ea683a09373f768281f8baa3d29ed2
coolsa <me@cloudchagnon.xyz>
2020-12-08 00:48:56 -0700

    SOMEHOW IT WORKS

--- src/main/java/org/openapitools/configuration/OpenAPIDocumentationConfig.java
+++ src/main/java/org/openapitools/configuration/OpenAPIDocumentationConfig.java
@@ -23,49 +23,43 @@ import javax.servlet.ServletContext;
 @EnableSwagger2
 public class OpenAPIDocumentationConfig {
 
-    ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-            .title("Air Schedual API")
-            .description("Communication API specifications for an aircraft schedule API, mainly for use by client.")
-            .license("Apache 2.0")
-            .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
-            .termsOfServiceUrl("")
-            .version("0.0.1")
-            .contact(new Contact("","", ""))
-            .build();
-    }
+	ApiInfo apiInfo() {
+		return new ApiInfoBuilder().title("Air Schedual API")
+				.description("Communication API specifications for an aircraft schedule API, mainly for use by client.")
+				.license("Apache 2.0").licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+				.termsOfServiceUrl("").version("0.0.1").contact(new Contact("", "", "")).build();
+	}
 
-    @Bean
-    public Docket customImplementation(ServletContext servletContext, @Value("${openapi.airSchedual.base-path:}") String basePath) {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                    .apis(RequestHandlerSelectors.basePackage("org.openapitools.api"))
-                    .build()
-                .pathProvider(new BasePathAwareRelativePathProvider(servletContext, basePath))
-                .directModelSubstitute(java.time.LocalDate.class, java.sql.Date.class)
-                .directModelSubstitute(java.time.OffsetDateTime.class, java.util.Date.class)
-                .apiInfo(apiInfo());
-    }
+	@Bean
+	public Docket customImplementation(ServletContext servletContext,
+			@Value("${openapi.airSchedual.base-path:}") String basePath) {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("org.openapitools.api")).build()
+				.pathProvider(new BasePathAwareRelativePathProvider(servletContext, basePath))
+				.directModelSubstitute(java.time.LocalDate.class, java.sql.Date.class)
+				.directModelSubstitute(java.time.OffsetDateTime.class, java.util.Date.class).apiInfo(apiInfo());
+	}
 
-    class BasePathAwareRelativePathProvider extends RelativePathProvider {
-        private String basePath;
+	class BasePathAwareRelativePathProvider extends RelativePathProvider {
+		private String basePath;
 
-        public BasePathAwareRelativePathProvider(ServletContext servletContext, String basePath) {
-            super(servletContext);
-            this.basePath = basePath;
-        }
+		public BasePathAwareRelativePathProvider(ServletContext servletContext, String basePath) {
+			super(servletContext);
+			this.basePath = basePath;
+		}
 
-        @Override
-        protected String applicationPath() {
-            return  Paths.removeAdjacentForwardSlashes(UriComponentsBuilder.fromPath(super.applicationPath()).path(basePath).build().toString());
-        }
+		@Override
+		protected String applicationPath() {
+			return Paths.removeAdjacentForwardSlashes(
+					UriComponentsBuilder.fromPath(super.applicationPath()).path(basePath).build().toString());
+		}
 
-        @Override
-        public String getOperationPath(String operationPath) {
-            UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder.fromPath("/");
-            return Paths.removeAdjacentForwardSlashes(
-                    uriComponentsBuilder.path(operationPath.replaceFirst("^" + basePath, "")).build().toString());
-        }
-    }
+		@Override
+		public String getOperationPath(String operationPath) {
+			UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder.fromPath("/");
+			return Paths.removeAdjacentForwardSlashes(
+					uriComponentsBuilder.path(operationPath.replaceFirst("^" + basePath, "")).build().toString());
+		}
+	}
 
 }
